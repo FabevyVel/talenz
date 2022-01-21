45dad82d9a2796f115ccd83eef64fc97ddd3723a
FabevyVel <vel@fabevy.com>
2022-01-21 09:29:14 +0530

    base-files.

---
+++ README.md
@@ -0,0 +1,1 @@
+# pro-recruiter-connect-static
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const include = require('gulp-include');
+const merge = require('merge-stream');
+const browserSync = require('browser-sync').create();
+
+
+//compile scss into css
+function style() {
+    //1.where is my scss
+    return gulp.src('src/scss/**/*.scss') //gets all files ending with .scss in src/scss
+    //2. pass that file through sass compiler
+    .pipe(sass().on('error',sass.logError))
+    //3. where do I save the compiled css file
+    .pipe(gulp.dest('src/css'))
+    //4. stream change to all browsers
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./src",
+            index: "/index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('src/**/*.html').on('change',browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
