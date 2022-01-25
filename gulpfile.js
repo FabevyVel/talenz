f79ecd613a64e040d354f0fdd029bb2c1c874f4e
monisharajesh <monisharajesh078@gmail.com>
2022-01-25 09:31:14 +0530

    Add my changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const include = require('gulp-include');
 const merge = require('merge-stream');
 const browserSync = require('browser-sync').create();
